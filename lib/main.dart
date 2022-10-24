0e92e205d71e5ee1de237d61a3c7b2a9e1643cc4
HuuTuong100101 <tuongb1910480@student.ctu.edu.vn>
2022-10-24 16:13:08 +0700

    Luu danh muc san pham vao Realtime Database cua Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -22,15 +22,23 @@ class MyApp extends StatelessWidget {
           ChangeNotifierProvider(
             create: (ctx) => AuthManager(),
           ),
-          ChangeNotifierProvider(
+
+          ChangeNotifierProxyProvider<AuthManager, ProductsManager>(
             create: (ctx) => ProductsManager(),
+            update: (ctx, authManager, productsManager) {
+              productsManager!.authToken = authManager.authToken;
+              return productsManager;
+            },
           ),
+
           ChangeNotifierProvider(
             create: (ctx) => CartManager(),
           ),
+
           ChangeNotifierProvider(
             create: (ctx) => OrdersManager(),
           ),
+          
         ],
         child: Consumer<AuthManager>(
           builder: (ctx, authManager, child) {
--- lib/ui/products/product_overview_screen.dart
+++ lib/ui/products/product_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myshop/models/product.dart';
 import 'package:myshop/ui/cart/cart_screen.dart';
+import 'package:myshop/ui/screens.dart';
 import 'products_grid.dart';
 import '../shared/app_drawer.dart';
 import '../cart/cart_manager.dart';
@@ -17,7 +18,14 @@ class ProductsOverviewScreen extends StatefulWidget {
 }
 
 class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
-  var _showOnlyFavorites = false;
+  final _showOnlyFavorites = ValueNotifier<bool>(false);
+  late Future<void> _fetchProducts;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchProducts = context.read<ProductsManager>().fetchProducts();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +38,20 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
         ],
       ),
       drawer: const AppDrawer(),
-      body: ProductsGrid(_showOnlyFavorites),
+      body: FutureBuilder(
+          future: _fetchProducts,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return ValueListenableBuilder(
+                  valueListenable: _showOnlyFavorites,
+                  builder: (context, onlyFavorites, child) {
+                    return ProductsGrid(onlyFavorites);
+                  });
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
     );
   }
 
@@ -55,9 +76,9 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
       onSelected: (FilterOptions selectedValue) {
         setState(() {
           if (selectedValue == FilterOptions.favorites) {
-            _showOnlyFavorites = true;
+            _showOnlyFavorites.value = true;
           } else {
-            _showOnlyFavorites = false;
+            _showOnlyFavorites.value = false;
           }
         });
       },
